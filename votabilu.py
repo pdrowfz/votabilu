682879f8ee022e145e65c56d507683853fe9d530
Hugo Nomura <hugonomura@gmail.com>
2014-08-23 15:18:18 -0300

    recuperando um candidato aleatorio do redis

--- votabilu.py
+++ votabilu.py
@@ -5,6 +5,7 @@ import cherrypy
 from _config import votabilu_conf as config
 from os import path
 from jinja2 import Environment, FileSystemLoader
+import redis
 
 env = Environment(loader = FileSystemLoader('static'))
 
@@ -13,5 +14,9 @@ class VotaBilu(object):
     def index(self):
         tmpl = env.get_template('index.html')
         return tmpl.render(title = 'Vota, Bilu!')
+
+    def get_candidate():
+        r = redis.StrictRedis(host='localhost', port=6379, db=0)
+        return r.srandmember('candidatos:SP')
 
 cherrypy.quickstart(VotaBilu(), '/', config.CHERRYPY_CONFIG)
